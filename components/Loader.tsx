1edf5ac00b4b81f16789c238e6e5b7ac0d552f55
Kshitij Patel <kshitijpatel54@gmail.com>
2025-06-18 09:45:00 +0530

    Improve loader UI with better messaging and dark background

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 
 const Loader = () => (
-  <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
+  <div className='flex h-screen w-screen flex-col items-center justify-center gap-3 bg-primary-black'>
     <Image
       src='/assets/loader.gif'
       alt='loader'
-      width={100}
-      height={100}
+      width={120}
+      height={120}
       className='object-contain'
     />
-    <p className='text-sm font-bold text-primary-grey-300'>Loading...</p>
+    <p className='text-sm font-bold text-primary-grey-300'>
+      Setting up your canvas...
+    </p>
+    <p className='text-xs text-primary-grey-300 opacity-60'>
+      Connecting to collaboration server
+    </p>
   </div>
 );
 
